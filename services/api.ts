609a74c3fa288dafee307821f96ce8b5b7463f9f
bariasp <benjamin.arias.pavez@ciisa.cl>
2026-05-21 20:08:45 -0400

    fix: se agrega interceptor para enviar token en requests

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.85:3000', // REVISAR IP LOCAL
+  baseURL: 'http://192.168.1.85:3000',
+});
+
+// Interceptor para agregar el token automáticamente
+api.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
